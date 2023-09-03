ca110b3ce2b067639d1924c174baa5668a6deffc
plastikSun <marcom2k01@gmail.com>
2023-09-03 12:08:55 +0200

    add: dynamic contact list (milestone 1, bullet 2)

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue 
+
+const Boolzapp = createApp({
+    data() {
+        return {
+            contacts: contacts,
+        }
+
+    },
+
+    method() {
+        
+    },
+
+    mounted() {
+        console.log(this.contacts)
+    }
+}).mount("#boolzapp")
