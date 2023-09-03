c67a2d718f24d5d01b860f7bb3a468b897255b81
plastikSun <marcom2k01@gmail.com>
2023-09-03 16:10:08 +0200

    complete: milestone 2 (todo: fix last msg tail)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
-const {createApp} = Vue 
+const { createApp } = Vue
 
 const Boolzapp = createApp({
     data() {
         return {
             contacts: contacts,
+            activeContact: {...contacts[0]},
+            lastMsg: undefined,
         }
 
     },
 
-    method() {
+    methods: {
+        showConvo(index) {
+            const reactive = this.contacts[index].messages
         
+            this.activeContact = {...this.contacts[index]}
+        
+            for (i = 0; i < reactive.length; i++) {
+        
+                const textMsg = reactive[i]
+        
+            }
+        
+        },
+        
+
+        isLastMsg(messages,currentIndex) {
+           if ((currentIndex) == messages.length - 1) {
+            return true
+           }
+        }
     },
 
-    mounted() {
-        console.log(this.contacts)
-    }
 }).mount("#boolzapp")
+
+
+
+
+
